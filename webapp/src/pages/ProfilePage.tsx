ba988c6785a48547df5ca4feb19657250aaac4e4
Daniel Winterstein <daniel.winterstein@n-able.com>
2026-02-03 11:43:19 +0000

    fix

--- webapp/src/pages/ProfilePage.tsx
+++ webapp/src/pages/ProfilePage.tsx
@@ -49,7 +49,7 @@ const ProfilePage: React.FC = () => {
           <Card>
             <CardHeader className="d-flex justify-content-between align-items-center">
               <h5 className="mb-0">Profile Information</h5>
-              {isSuperAdmin && <Badge color="primary">Super Admin</Badge>}
+              {user.isSuperAdmin && <Badge color="primary">Super Admin</Badge>}
             </CardHeader>
             <CardBody>
               {auth0User?.picture && (
